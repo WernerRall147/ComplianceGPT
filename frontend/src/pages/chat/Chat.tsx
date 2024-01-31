0385512e01942f1f87700bcf36a4ec0ac3f90edd
Werner Rall <weral@microsoft.com>
2024-01-31 14:46:05 +0200

    Updated Main Logo and Changed the wording of Start Chatting

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -607,8 +607,8 @@ const Chat = () => {
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
-                                <h1 className={styles.chatEmptyStateTitle}>Start chatting</h1>
-                                <h2 className={styles.chatEmptyStateSubtitle}>This chatbot is configured to answer your questions</h2>
+                                <h1 className={styles.chatEmptyStateTitle}>Ask your Compliance Question</h1>
+                                <h2 className={styles.chatEmptyStateSubtitle}>This chatbot is configured to answer your questions regarding compliance</h2>
                             </Stack>
                         ) : (
                             <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? "40px" : "0px" }} role="log">
